32b316868d6425bbfac88fe99c4afa228c975473
GThinh17 <trangiathinh0309@gmail.com>
2025-12-08 13:04:40 +0700

    add

--- 2-back-end-javaspring/src/main/java/vn/gt/__back_end_javaspring/mapper/ReviewerMapper.java
+++ 2-back-end-javaspring/src/main/java/vn/gt/__back_end_javaspring/mapper/ReviewerMapper.java
@@ -1,19 +1,19 @@
-    package vn.gt.__back_end_javaspring.mapper;
+package vn.gt.__back_end_javaspring.mapper;
 
-    import org.mapstruct.Mapper;
-    import org.mapstruct.Mapping;
-    import vn.gt.__back_end_javaspring.DTO.ReviewerCreateDTO;
-    import vn.gt.__back_end_javaspring.DTO.ReviewerResponse;
-    import vn.gt.__back_end_javaspring.entity.Reviewer;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import vn.gt.__back_end_javaspring.DTO.ReviewerCreateDTO;
+import vn.gt.__back_end_javaspring.DTO.ReviewerResponse;
+import vn.gt.__back_end_javaspring.entity.Reviewer;
 
-    @Mapper(componentModel = "spring")
-    public interface ReviewerMapper {
+@Mapper(componentModel = "spring")
+public interface ReviewerMapper {
 
-        Reviewer toModel(ReviewerCreateDTO reviewerCreateDTO);
+    Reviewer toModel(ReviewerCreateDTO reviewerCreateDTO);
 
-        @Mapping(source = "user.id", target = "userId")
-        @Mapping(source = "user.fullName", target = "userName")
-        @Mapping(source = "user.avatar", target = "userAvatarUrl")
-        @Mapping(source = "user.email", target = "userEmail")
-        ReviewerResponse toResponse(Reviewer reviewer);
-    }
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "user.fullName", target = "userName")
+    @Mapping(source = "user.avatar", target = "userAvatarUrl")
+    @Mapping(source = "user.email", target = "userEmail")
+    ReviewerResponse toResponse(Reviewer reviewer);
+}
