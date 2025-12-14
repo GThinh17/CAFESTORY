120fea9edf257f8006dc95077ff1974a6f0f0758
Anh Phan <anhphan42891@gmail.com>
2025-12-14 22:10:18 +0700

    adding before pull

---
+++ 2-back-end-javaspring/target/generated-sources/annotations/vn/gt/__back_end_javaspring/mapper/CommentMapperImpl.java
@@ -0,0 +1,204 @@
+package vn.gt.__back_end_javaspring.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import vn.gt.__back_end_javaspring.DTO.CommentCreateDTO;
+import vn.gt.__back_end_javaspring.DTO.CommentResponse;
+import vn.gt.__back_end_javaspring.DTO.CommentUpdateDTO;
+import vn.gt.__back_end_javaspring.entity.Blog;
+import vn.gt.__back_end_javaspring.entity.Comment;
+import vn.gt.__back_end_javaspring.entity.CommentImage;
+import vn.gt.__back_end_javaspring.entity.User;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-12-14T22:08:19+0700",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.9 (Ubuntu)"
+)
+@Component
+public class CommentMapperImpl implements CommentMapper {
+
+    @Override
+    public CommentResponse toResponse(Comment comment) {
+        if ( comment == null ) {
+            return null;
+        }
+
+        CommentResponse.CommentResponseBuilder commentResponse = CommentResponse.builder();
+
+        commentResponse.commentId( comment.getId() );
+        commentResponse.blogId( commentBlogId( comment ) );
+        commentResponse.parentCommentId( commentParentCommentId( comment ) );
+        commentResponse.userId( commentUserId( comment ) );
+        commentResponse.userFullName( commentUserFullName( comment ) );
+        commentResponse.userAvatar( commentUserAvatar( comment ) );
+        commentResponse.commentImageId( commentCommentImageId( comment ) );
+        commentResponse.commentImageUrl( commentCommentImageImageUrl( comment ) );
+        commentResponse.content( comment.getContent() );
+        commentResponse.likesCount( comment.getLikesCount() );
+        commentResponse.replyCount( comment.getReplyCount() );
+        commentResponse.isEdited( comment.getIsEdited() );
+        commentResponse.isDeleted( comment.getIsDeleted() );
+        commentResponse.isPin( comment.getIsPin() );
+        commentResponse.createdAt( comment.getCreatedAt() );
+        commentResponse.updatedAt( comment.getUpdatedAt() );
+        commentResponse.deletedAt( comment.getDeletedAt() );
+        commentResponse.replies( toResponseList( comment.getReplies() ) );
+
+        return commentResponse.build();
+    }
+
+    @Override
+    public List<CommentResponse> toResponseList(List<Comment> comments) {
+        if ( comments == null ) {
+            return null;
+        }
+
+        List<CommentResponse> list = new ArrayList<CommentResponse>( comments.size() );
+        for ( Comment comment : comments ) {
+            list.add( toResponse( comment ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public Comment toModel(CommentCreateDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Comment comment = new Comment();
+
+        comment.setBlog( commentCreateDTOToBlog( dto ) );
+        comment.setUser( commentCreateDTOToUser( dto ) );
+        comment.setParentComment( commentCreateDTOToComment( dto ) );
+        comment.setCommentImage( commentCreateDTOToCommentImage( dto ) );
+        comment.setContent( dto.getContent() );
+
+        return comment;
+    }
+
+    @Override
+    public void updateEntity(Comment entity, CommentUpdateDTO dto) {
+        if ( dto == null ) {
+            return;
+        }
+
+        if ( dto.getContent() != null ) {
+            entity.setContent( dto.getContent() );
+        }
+        if ( dto.getIsDeleted() != null ) {
+            entity.setIsDeleted( dto.getIsDeleted() );
+        }
+        if ( dto.getIsPin() != null ) {
+            entity.setIsPin( dto.getIsPin() );
+        }
+    }
+
+    private String commentBlogId(Comment comment) {
+        Blog blog = comment.getBlog();
+        if ( blog == null ) {
+            return null;
+        }
+        return blog.getId();
+    }
+
+    private String commentParentCommentId(Comment comment) {
+        Comment parentComment = comment.getParentComment();
+        if ( parentComment == null ) {
+            return null;
+        }
+        return parentComment.getId();
+    }
+
+    private String commentUserId(Comment comment) {
+        User user = comment.getUser();
+        if ( user == null ) {
+            return null;
+        }
+        return user.getId();
+    }
+
+    private String commentUserFullName(Comment comment) {
+        User user = comment.getUser();
+        if ( user == null ) {
+            return null;
+        }
+        return user.getFullName();
+    }
+
+    private String commentUserAvatar(Comment comment) {
+        User user = comment.getUser();
+        if ( user == null ) {
+            return null;
+        }
+        return user.getAvatar();
+    }
+
+    private String commentCommentImageId(Comment comment) {
+        CommentImage commentImage = comment.getCommentImage();
+        if ( commentImage == null ) {
+            return null;
+        }
+        return commentImage.getId();
+    }
+
+    private String commentCommentImageImageUrl(Comment comment) {
+        CommentImage commentImage = comment.getCommentImage();
+        if ( commentImage == null ) {
+            return null;
+        }
+        return commentImage.getImageUrl();
+    }
+
+    protected Blog commentCreateDTOToBlog(CommentCreateDTO commentCreateDTO) {
+        if ( commentCreateDTO == null ) {
+            return null;
+        }
+
+        Blog blog = new Blog();
+
+        blog.setId( commentCreateDTO.getBlogId() );
+
+        return blog;
+    }
+
+    protected User commentCreateDTOToUser(CommentCreateDTO commentCreateDTO) {
+        if ( commentCreateDTO == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setId( commentCreateDTO.getUserId() );
+
+        return user;
+    }
+
+    protected Comment commentCreateDTOToComment(CommentCreateDTO commentCreateDTO) {
+        if ( commentCreateDTO == null ) {
+            return null;
+        }
+
+        Comment comment = new Comment();
+
+        comment.setId( commentCreateDTO.getCommentParentId() );
+
+        return comment;
+    }
+
+    protected CommentImage commentCreateDTOToCommentImage(CommentCreateDTO commentCreateDTO) {
+        if ( commentCreateDTO == null ) {
+            return null;
+        }
+
+        CommentImage commentImage = new CommentImage();
+
+        commentImage.setImageUrl( commentCreateDTO.getCommentImageUrl() );
+
+        return commentImage;
+    }
+}
---
+++ 2-back-end-javaspring/target/generated-sources/annotations/vn/gt/__back_end_javaspring/mapper/PageMapperImpl.java
@@ -0,0 +1,128 @@
+package vn.gt.__back_end_javaspring.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import vn.gt.__back_end_javaspring.DTO.PageCreateDTO;
+import vn.gt.__back_end_javaspring.DTO.PageResponse;
+import vn.gt.__back_end_javaspring.DTO.PageUpdateDTO;
+import vn.gt.__back_end_javaspring.entity.CafeOwner;
+import vn.gt.__back_end_javaspring.entity.Page;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-12-14T22:08:19+0700",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.9 (Ubuntu)"
+)
+@Component
+public class PageMapperImpl implements PageMapper {
+
+    @Override
+    public Page toModel(PageCreateDTO request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        Page page = new Page();
+
+        page.setPageName( request.getPageName() );
+        page.setSlug( request.getSlug() );
+        page.setDescription( request.getDescription() );
+        page.setAvatarUrl( request.getAvatarUrl() );
+        page.setCoverUrl( request.getCoverUrl() );
+        page.setContactPhone( request.getContactPhone() );
+        page.setContactEmail( request.getContactEmail() );
+        page.setOpenHours( map( request.getOpenHours() ) );
+
+        return page;
+    }
+
+    @Override
+    public PageResponse toResponse(Page page) {
+        if ( page == null ) {
+            return null;
+        }
+
+        PageResponse.PageResponseBuilder pageResponse = PageResponse.builder();
+
+        pageResponse.pageId( page.getId() );
+        pageResponse.businessName( pageCafeOwnerBusinessName( page ) );
+        pageResponse.cafeOwnerId( pageCafeOwnerId( page ) );
+        pageResponse.pageName( page.getPageName() );
+        pageResponse.postCount( page.getPostCount() );
+        pageResponse.slug( page.getSlug() );
+        pageResponse.followingCount( page.getFollowingCount() );
+        pageResponse.description( page.getDescription() );
+        pageResponse.avatarUrl( page.getAvatarUrl() );
+        pageResponse.coverUrl( page.getCoverUrl() );
+        pageResponse.contactPhone( page.getContactPhone() );
+        pageResponse.contactEmail( page.getContactEmail() );
+        pageResponse.isVerified( page.getIsVerified() );
+        pageResponse.verifiedAt( page.getVerifiedAt() );
+        pageResponse.openHours( page.getOpenHours() );
+        pageResponse.createdAt( page.getCreatedAt() );
+        pageResponse.updatedAt( page.getUpdatedAt() );
+
+        return pageResponse.build();
+    }
+
+    @Override
+    public List<PageResponse> toResponse(List<Page> pages) {
+        if ( pages == null ) {
+            return null;
+        }
+
+        List<PageResponse> list = new ArrayList<PageResponse>( pages.size() );
+        for ( Page page : pages ) {
+            list.add( toResponse( page ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public void updateEntity(Page page, PageUpdateDTO request) {
+        if ( request == null ) {
+            return;
+        }
+
+        if ( request.getPageName() != null ) {
+            page.setPageName( request.getPageName() );
+        }
+        if ( request.getDescription() != null ) {
+            page.setDescription( request.getDescription() );
+        }
+        if ( request.getAvatarUrl() != null ) {
+            page.setAvatarUrl( request.getAvatarUrl() );
+        }
+        if ( request.getCoverUrl() != null ) {
+            page.setCoverUrl( request.getCoverUrl() );
+        }
+        if ( request.getContactPhone() != null ) {
+            page.setContactPhone( request.getContactPhone() );
+        }
+        if ( request.getContactEmail() != null ) {
+            page.setContactEmail( request.getContactEmail() );
+        }
+        if ( request.getOpenHours() != null ) {
+            page.setOpenHours( map( request.getOpenHours() ) );
+        }
+    }
+
+    private String pageCafeOwnerBusinessName(Page page) {
+        CafeOwner cafeOwner = page.getCafeOwner();
+        if ( cafeOwner == null ) {
+            return null;
+        }
+        return cafeOwner.getBusinessName();
+    }
+
+    private String pageCafeOwnerId(Page page) {
+        CafeOwner cafeOwner = page.getCafeOwner();
+        if ( cafeOwner == null ) {
+            return null;
+        }
+        return cafeOwner.getId();
+    }
+}
