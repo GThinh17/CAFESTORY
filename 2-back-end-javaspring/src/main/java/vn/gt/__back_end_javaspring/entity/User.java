629f3bca4990a95715a86d106a6804561227f07c
Anh Phan <anhphan42891@gmail.com>
2025-12-16 16:43:27 +0700

    resolve conflict3

--- 2-back-end-javaspring/src/main/java/vn/gt/__back_end_javaspring/entity/User.java
+++ 2-back-end-javaspring/src/main/java/vn/gt/__back_end_javaspring/entity/User.java
@@ -64,7 +64,7 @@ public class User { // Check
     @Column(name = "fcm_token")
 
     private String fcmToken; // Neu ma dùng tren nhieu thiet bi thi se tao bang rieng
-
+    
     @Column(name = "location")
     private String location;
 
